b5bfe75d402492202fab5324ae72ebc95d27be0c
OwadaKazuhiro <wadaoo0223@icloud.com>
2020-12-07 15:34:49 +0900

    [add] 問い合わせフォームページの作成

--- schedule/manage_schedule/urls.py
+++ schedule/manage_schedule/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'manage_schedule'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('inquiry/', views.InquiryView.as_view(),name='inquiry'),
 ]
--- schedule/manage_schedule/views.py
+++ schedule/manage_schedule/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from .forms import InquiryForm
 # Create your views here.
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = 'inquiry.html'
+    form_class = InquiryForm
