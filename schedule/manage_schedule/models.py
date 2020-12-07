a92d100d8c388571492a9097acce916284a5676c
OwadaKazuhiro <wadaoo0223@icloud.com>
2020-12-07 17:53:13 +0900

    [create] スケジュール管理モデルの作成

--- schedule/manage_schedule/models.py
+++ schedule/manage_schedule/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+
+class Schedule(models.Model):
+    """スケジュール管理モデル"""
+
+    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
+    title = models.CharField(verbose_name='タイトル', max_length=40)
+    content = models.TextField(verbose_name='本文', blank=True, null=True)
+    photo1 = models.ImageField(verbose_name='写真1', blank=True, null=True)
+    photo2 = models.ImageField(verbose_name='写真2', blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name='作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日時', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Schedule"
+
+    def __str__(self):
+        return self.title
